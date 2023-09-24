54eee21250200d91069024772388842fedd41590
Toni Pietilä <toni.pietila@student.hamk.fi>
2023-09-24 22:11:31 +0300

    Lisätty kommentteja
    Lisätty kommentteja selkiyttämään koodin lukemista.

--- GuessingGameGit.java
+++ GuessingGameGit.java
@@ -6,31 +6,31 @@ public class GuessingGameGit {
 
 		Scanner in = new Scanner(System.in);
 		
-		int numberOfGuesses = 0 ; 
+		int numberOfGuesses = 0 ; //Muuttuja arvausten määrälle laskuria varten, pelin alussa 0.
 		boolean gameIsRunning;
 		
-		do		{  
+		do		{  	//Aloitetaan looppi.
 		System.out.println("What's my name? Write END if you want to leave the game.");
-		String name = in.nextLine(); 
+		String name = in.nextLine(); //Käyttäjän input tallennetaan muuttujaan "name".
 		
-				if (name.equals("Danielle")) { 
-					numberOfGuesses++; 
+				if (name.equals("Danielle")) { //Määritellään oikea vastaus.
+					numberOfGuesses++; //Sisällytetään oikean nimen arvaus laskuriin.
 					System.out.println("Congrats, that's correct! You got it right in " + numberOfGuesses + " tries!");
-					gameIsRunning = false; 
+					gameIsRunning = false; //Käyttäjä arvaa oikein, ohjelma päättyy.
 					 
 				}
 		
 				else if (name.equals("END")) { 
 					System.out.println("You quit.\n" + "You tried " + numberOfGuesses + " times.");
-					gameIsRunning = false;
+					gameIsRunning = false; //Käyttäjä lopettaa ohjelman. Yritysten määrä tulostetaan. ENDin kirjoittamista ei lasketa yritykseksi.
 					
 				}
 		
-				else {
+				else {	//Jos käyttäjä ei kirjoita oikeaa vastausta tai lopeta ohjelmaa, looppi jatkaa pyörimistään.
 	
 					System.out.println("Wrong! Try again.");
-					gameIsRunning = true;
-					numberOfGuesses++; 
+					gameIsRunning = true; //Peli jatkaa pyörimistään.
+					numberOfGuesses++; //Lasketaan arvauskertoja, jos arvaus menee väärin.
 				}
 		} while (gameIsRunning);
 	}
