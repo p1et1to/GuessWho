3fb5ef2527aa1d1691924dd7dab07708ba4a763f
Toni Pietilä <toni.pietila@student.hamk.fi>
2023-09-24 22:08:18 +0300

    2. versio muutoksilla
    Lisätty boolean arvo pelin "käymiselle", jotta koodi itselle helpompilukuista. Looppia ei päätetä enää breakilla, vaan booleanin arvon muuttamisella. Itselle selkeämpi tapa käsittää, tosin en tiedä onko sen parempaa koodia. :)

--- GuessingGameGit.java
+++ GuessingGameGit.java
@@ -3,32 +3,35 @@ import java.util.Scanner;
 public class GuessingGameGit {
 
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		
 		int numberOfGuesses = 0 ; 
-				
+		boolean gameIsRunning;
+		
 		do		{  
 		System.out.println("What's my name? Write END if you want to leave the game.");
-		String name = in.nextLine();
+		String name = in.nextLine(); 
 		
-				if (name.equals("Danielle"))  { 
-					numberOfGuesses++;
+				if (name.equals("Danielle")) { 
+					numberOfGuesses++; 
 					System.out.println("Congrats, that's correct! You got it right in " + numberOfGuesses + " tries!");
-					break;
+					gameIsRunning = false; 
+					 
 				}
 		
 				else if (name.equals("END")) { 
 					System.out.println("You quit.\n" + "You tried " + numberOfGuesses + " times.");
-					break;
+					gameIsRunning = false;
 					
 				}
 		
 				else {
 	
 					System.out.println("Wrong! Try again.");
+					gameIsRunning = true;
 					numberOfGuesses++; 
-			}
-		} while (true);
+				}
+		} while (gameIsRunning);
 	}
 }
